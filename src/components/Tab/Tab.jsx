f4830fe8b17c2188c946b81af222af53c535dd04
Max <88189468+maker0101@users.noreply.github.com>
2022-03-15 16:15:22 +0100

    Unify approach to assign classes

--- src/components/Tab/Tab.jsx
+++ src/components/Tab/Tab.jsx
@@ -1,6 +1,7 @@
 import './Tab.css';
 import { useContext } from 'react';
 import { ModalContext } from '../../contexts/ModalContext';
+import classNames from 'classnames';
 
 const Tab = ({ data }) => {
   const { activeTab, setActiveTab } = useContext(ModalContext);
@@ -8,24 +9,34 @@ const Tab = ({ data }) => {
   return (
     <div className='Tab'>
       <div className='tabBtn__wrapper'>
-        {data.map((tab) => (
-          <div
-            key={tab.index}
-            className={`tabBtn ${activeTab === tab.index && 'tabBtn__active'}`}
-            onClick={() => setActiveTab(tab.index)}>
-            {tab.name}
-          </div>
-        ))}
+        {data.map((tab) => {
+          const tabBtnClasses = classNames({
+            tabBtn: true,
+            tabBtn__active: activeTab === tab.index,
+          });
+
+          return (
+            <div
+              key={tab.index}
+              className={tabBtnClasses}
+              onClick={() => setActiveTab(tab.index)}>
+              {tab.name}
+            </div>
+          );
+        })}
       </div>
-      {data.map((tab) => (
-        <div
-          key={tab.index}
-          className={`tabContent ${
-            activeTab === tab.index && 'tabContent__active'
-          }`}>
-          <div>{tab.content}</div>
-        </div>
-      ))}
+      {data.map((tab) => {
+        const tabContent = classNames({
+          tabContent: true,
+          tabContent__active: activeTab === tab.index,
+        });
+
+        return (
+          <div key={tab.index} className={tabContent}>
+            <div>{tab.content}</div>
+          </div>
+        );
+      })}
     </div>
   );
 };
