eb136415192e7e89dbfedbd05343e2dfc9ee91ae
Max <88189468+maker0101@users.noreply.github.com>
2022-03-22 15:30:33 +0100

    Update fiat balance working

--- src/components/TransactionForm/TransactionForm.jsx
+++ src/components/TransactionForm/TransactionForm.jsx
@@ -1,10 +1,11 @@
 import { useContext } from 'react';
 import { SelectAssetContext } from '../../contexts/SelectAssetContext';
 import useAssets from '../../hooks/useAssets';
-import { addDoc, collection } from 'firebase/firestore';
+import { addDoc, updateDoc, collection, doc } from 'firebase/firestore';
 import { db } from '../../firebase-config';
 import { createTransaction } from '../../utilities/create-transaction';
 import { validateTransaction } from '../../utilities/validate-transaction';
+import { findAsset } from '../../utilities/find-asset';
 
 const TransactionForm = ({ children, type }) => {
   const { allCoins, yourCoins, allFiat } = useAssets();
@@ -25,7 +26,38 @@ const TransactionForm = ({ children, type }) => {
     }
   };
 
-  const updateFiat = () => console.log('update fiat');
+  const updateYourFiat = async (transaction, fiatAssets) => {
+    try {
+      const fiatId = 'alClp2ZFuQBTf5Ljo3MA';
+      const fiatDoc = doc(db, 'yourFiat', fiatId);
+
+      const yourFiatBalance = fiatAssets[0].balance_eur;
+      let fiatBalanceChange;
+
+      switch (transaction.type) {
+        case 'buyCoin':
+        case 'cashoutFiat':
+          fiatBalanceChange = -transaction.amount_fiat;
+          break;
+        case 'sellCoin':
+        case 'convertCoin':
+        case 'sendCoin':
+        case 'depositFiat':
+          fiatBalanceChange = transaction.amount_fiat;
+          break;
+        default:
+          console.error('Unknown transaction type');
+          break;
+      }
+      const newFiatBalance = yourFiatBalance + fiatBalanceChange;
+
+      await updateDoc(fiatDoc, {
+        balance_eur: newFiatBalance,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
   const updateCoins = () => console.log('update coins');
   const clearForm = () => console.log('clear form');
   const showError = (error) => console.error(error);
@@ -37,7 +69,7 @@ const TransactionForm = ({ children, type }) => {
 
     if (result?.isValid) {
       addTransaction(transaction);
-      updateFiat(transaction);
+      updateYourFiat(transaction, allFiat);
       updateCoins(transaction);
       clearForm();
     } else {
