ac666f5512d7ef2334459be45fe1bfc4ddd5cc7c
Max <88189468+maker0101@users.noreply.github.com>
2022-03-29 12:32:23 +0200

    Adjust timeframes for chart

--- src/utilities/create-chart-times.js
+++ src/utilities/create-chart-times.js
@@ -2,29 +2,27 @@ import dayjs from 'dayjs';
 
 export const createChartTimes = () => {
   const times = {
-    '1H': [],
     '1D': [],
     '1W': [],
     '1M': [],
     '1Y': [],
   };
 
-  let stepSize = 60 / 6;
-  let offset;
-  for (let i = 60; i > 0; i -= 60 / 6) {
-    const time = dayjs().subtract(i, 'minute').format('H:mm A');
-    times['1H'].push(time);
-  }
-
-  stepSize = 24 / 6;
+  let stepSize = 24 / 6;
+  let offset = 2;
   for (let i = 24 - stepSize; i >= 0; i -= stepSize) {
-    const time = dayjs().subtract(i, 'hour').format('H:mm A');
+    const time = dayjs()
+      .subtract(i + offset, 'hour')
+      .format('H:mm A');
     times['1D'].push(time);
   }
 
   stepSize = 1;
+  offset = 16;
   for (let i = 6 - stepSize; i >= 0; i -= stepSize) {
-    const time = dayjs().subtract(i, 'day').format('MMM DD');
+    const time = dayjs()
+      .subtract(i * 24 + offset, 'hour')
+      .format('MMM DD');
     times['1W'].push(time);
   }
 
